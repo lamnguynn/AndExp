3e719edf442ee10b6b54b6ef28313306f6a8c4b2
Lam Nguyen <lance66nguyen@gmail.com>
2023-01-13 12:25:19 -0600

    (wip) added spotify sdk integration

--- app/src/main/java/com/example/andexp/MainActivity.java
+++ app/src/main/java/com/example/andexp/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.andexp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,7 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
     TextView nameLabel;
-    int numClicked = 0;
+
+    private static final String CLIENT_ID = "your_client_id";
+    private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
+    private ConnectionParams connectionParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupAssets();
+        setupAuth();
     }
 
     public void setupAssets() {
@@ -41,9 +47,47 @@ public class MainActivity extends AppCompatActivity {
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                numClicked += 1;
-                nameLabel.setText(Integer.toString(numClicked));
+                showSpotifyAuth();
             }
         });
+    }
+
+    public void setupAuth() {
+        connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+    }
+
+    private void showSpotifyAuth() {
+        //Check to see if spotify is downloaded before providing auth
+        if( SpotifyAppRemote.isSpotifyInstalled(this) ) {
+
+            //Connect to spotify to provide auth
+            SpotifyAppRemote.connect(this, connectionParams,
+                    new Connector.ConnectionListener() {
+            
+                        @Override
+                        public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                            mSpotifyAppRemote = spotifyAppRemote;
+                            Log.d("MainActivity", "Connected! Yay!");
+
+                            // Now you can start interacting with App Remote
+                            System.out.println("Connected....");
+                        }
+
+                        @Override
+                        public void onFailure(Throwable throwable) {
+                            Log.e("MainActivity", throwable.getMessage(), throwable);
+
+                            // Something went wrong when attempting to connect! Handle errors here
+                        }
+                    });
+        }
+        else {
+            Log.d("MainActivity", "Spotify NOT installed");
+        }
     }
 }
