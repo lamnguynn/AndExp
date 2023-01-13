edcb0b7d54bb868b078848f1db57760d823fd467
Lam Nguyen <lance66nguyen@gmail.com>
2023-01-13 12:02:16 -0600

    (wip) added more code

--- app/src/main/java/com/example/andexp/MainActivity.java
+++ app/src/main/java/com/example/andexp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.andexp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -10,19 +14,36 @@ public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
     TextView nameLabel;
+    int numClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {}
+
         setContentView(R.layout.activity_main);
 
         setupAssets();
     }
 
     public void setupAssets() {
+        nameLabel = findViewById(R.id.titleLabel);
+        nameLabel.setText("");
+
         submitButton = findViewById(R.id.submitButton);
         submitButton.setText("Submit");
 
-        nameLabel = findViewById(R.id.titleLabel);
+        //submitButton.setBackgroundColor(ContextCompat.getColor(this, R.color.black));
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numClicked += 1;
+                nameLabel.setText(Integer.toString(numClicked));
+            }
+        });
     }
 }
