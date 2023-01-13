0fac8d22c53ec71f2a5199145c1adc4e42716ae9
Lam Nguyen <lance66nguyen@gmail.com>
2023-01-13 11:28:37 -0600

    added starter code

--- app/src/main/java/com/example/andexp/MainActivity.java
+++ app/src/main/java/com/example/andexp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.andexp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
+    TextView nameLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupAssets();
+    }
+
+    public void setupAssets() {
+        submitButton = findViewById(R.id.submitButton);
+        submitButton.setText("Submit");
+
+        nameLabel = findViewById(R.id.titleLabel);
     }
 }
